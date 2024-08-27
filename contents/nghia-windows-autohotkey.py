a79d4f79d6db9a0dcbc97f16602e6c1a542a2dfe
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-27 09:38:02 +0700

    update nghia-windows-autohotkey.py

--- contents/nghia-windows-autohotkey.py
+++ contents/nghia-windows-autohotkey.py
@@ -1,38 +1,33 @@
 import os
-import subprocess
-import sys
-import ctypes
 
 
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
 
 
-if is_admin():
-    # Đường dẫn đến thư mục Startup
-    startup = os.path.join(os.environ['APPDATA'], r'Microsoft\Windows\Start Menu\Programs\Startup')
-    print(f"🚀 {startup}")
 
-    nghia_ahk_link = os.path.join(startup, "nghia.ahk")
-    print(f"🚀 {nghia_ahk_link}")
 
-    # Đường dẫn đến file nghia.ahk
-    nghia_ahk = os.path.join(os.getcwd(), "nghia.ahk")
-    print(f"🚀 {nghia_ahk}")
 
-    # Tạo liên kết
-    try:
-        subprocess.run(['mklink', nghia_ahk_link, nghia_ahk], shell=True, check=True)
-        print(f"Tạo liên kết thành công: {nghia_ahk_link}")
-    except subprocess.CalledProcessError as e:
-        print(f"Lỗi khi tạo liên kết: {e}")
-    except FileExistsError:
-        print("Liên kết đã tồn tại")
-    except OSError as e:
-        print(f"Lỗi hệ thống khi tạo liên kết: {e}")
-else:
-    ctypes.windll.shell32.ShellExecuteW(
-        None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
+
+
+
+# Đường dẫn đến thư mục Startup
+startup = os.path.join(os.environ['APPDATA'], r'Microsoft\Windows\Start Menu\Programs\Startup')
+print(f"🚀 {startup}")
+
+nghia_ahk_link = os.path.join(startup, "nghia-windows-autohotkey.ahk")
+print(f"🚀 {nghia_ahk_link}")
+
+nghia_ahk = os.path.join(os.getcwd(), "nghia-windows-autohotkey.ahk")
+print(f"🚀 {nghia_ahk}")
+
+
+
+
+try:
+    if os.path.exists(nghia_ahk_link):
+        os.remove(nghia_ahk_link)
+        print(f"Đã xóa symlink: {nghia_ahk_link}")
+    os.symlink(os.path.abspath(nghia_ahk), nghia_ahk_link)
+    print(f"Tạo link: {nghia_ahk_link}")
+except OSError as e:
+    print(f"Lỗi: {e}")
