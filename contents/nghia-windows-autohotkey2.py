388dd7325e0e92a2cfb6094bd4a8af9966442c33
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-21 08:37:47 +0700

    update 1 file, copy 1 file and create 1 file

---
+++ contents/nghia-windows-autohotkey2.py
@@ -0,0 +1,23 @@
+import os
+
+
+# Đường dẫn đến thư mục Startup
+startup = os.path.join(os.environ['APPDATA'], r'Microsoft\Windows\Start Menu\Programs\Startup')
+print(f"🚀 {startup}")
+
+nghia_ahk_link = os.path.join(startup, "temp")
+nghia_ahk_link = os.path.join(nghia_ahk_link, "nghia-windows-autohotkey2.ahk")
+print(f"🚀 {nghia_ahk_link}")
+
+nghia_ahk = os.path.join(os.getcwd(), "nghia-windows-autohotkey2.ahk")
+print(f"🚀 {nghia_ahk}")
+
+
+try:
+    if os.path.exists(nghia_ahk_link):
+        os.remove(nghia_ahk_link)
+        print(f"Đã xóa symlink: {nghia_ahk_link}")
+    os.symlink(os.path.abspath(nghia_ahk), nghia_ahk_link)
+    print(f"Tạo link: {nghia_ahk_link}")
+except OSError as e:
+    print(f"Lỗi: {e}")
